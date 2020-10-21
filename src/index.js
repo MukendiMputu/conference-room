0155735ab882a4fed4c1a951108df1f9c4f6a05b
MukendiMputu <mputusam@live.com>
2020-10-21 19:08:08 +0200

    add multiple changes to overwrite the default temp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './Components/App';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'popper.js/dist/umd/popper.js'
+import 'jquery/dist/jquery.js'
+import 'bootstrap/dist/js/bootstrap.js'
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", { style: {color: "red"} }, "Hello!") ,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
