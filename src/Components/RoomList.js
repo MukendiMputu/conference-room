a3f88ef69bd7440d03fb54b0488e69f88fe3654a
MukendiMputu <mputusam@live.com>
2020-11-26 20:34:58 +0100

    corrects the unique key warning

--- src/Components/RoomList.js
+++ src/Components/RoomList.js
@@ -5,7 +5,8 @@ class RoomList extends Component {
     constructor () {
         super();
         this.state = {
-            myrooms : []
+            myrooms : [],
+            lastIdx: 0
         };
     }
 
@@ -14,6 +15,8 @@ class RoomList extends Component {
         .then(response => response.json())
         .then(result => {
             const rooms = result.map(item => {
+                item.roomID = this.state.lastIdx;
+                this.setState({lastIdx: this.state.lastIdx + 1});
                 return item;
             });
             this.setState({
@@ -23,20 +26,6 @@ class RoomList extends Component {
     }
 
     render(){
-        const roomObjects = this.state.myrooms.map((item) => (
-          <div>
-            <div className="card">
-              <div className="card-header ch6"></div>
-              <div className="card-body">
-                <div className="card-title">{item.name}</div>
-                <div className="card-sub-title">Capacity: {item.capacity}</div>
-                <div className="card-desc">
-                  {item.description}
-                </div>
-              </div>
-            </div>
-          </div>
-        ));
 
         return (
           <div className="container-width">
@@ -47,7 +36,22 @@ class RoomList extends Component {
               and with free responsivity
             </div>
             <div className="cards">
-              {roomObjects}
+              {
+                this.state.myrooms.map((item) => (
+                    <div key={item.roomID}>
+                        <div className="card">
+                        <div className="card-header ch6"></div>
+                        <div className="card-body">
+                            <div className="card-title">{item.name}</div>
+                            <div className="card-sub-title">Capacity: {item.capacity}</div>
+                            <div className="card-desc">
+                            {item.description}
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    ))
+                }
             </div>
           </div>
         );
