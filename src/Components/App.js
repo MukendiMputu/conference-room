cb75341294d4bc31495e28492a196f1feb9c0bbe
MukendiMputu <mputusam@live.com>
2020-11-30 11:09:56 +0100

    modifies App from functional to class component
    To utilize the advantages of the class state

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
 import HeaderNavBar from './HeaderNavBar';
 import Footer from './Footer';
@@ -7,40 +7,48 @@ import RoomList from './RoomList';
 import Features from './Features';
 import BoardMembers from './BoardMembers';
 
-function App() {
-  return (
-      <div>
-      <HeaderNavBar/>
-      <MainHeader/>
-        <section className="flex-sect">
-            <RoomList />
-        </section>
+class App extends Component {
+    constructor () {
+        super();
+        this.state = {
+            
+        }
+    }
+    render () {
+        return (
+            <div>
+            <HeaderNavBar/>
+            <MainHeader/>
+            <section className="flex-sect">
+                <RoomList />
+            </section>
 
-        <section className="am-sect">
-            <div className="container-width">
-                <div className="am-container">
-                    <img alt="" onMouseDown={()=>{ return false; }} src="./img/phone-app.png" className="img-phone" />
-                    <div className="am-content">
-                        <div className="am-pre">ASSET MANAGER</div>
-                        <div className="am-title">Manage your images with Asset Manager</div>
-                        <div className="am-desc">You can create image blocks with the command from the left panel and edit them with double click</div>
-                        <div className="am-post">Image uploading is not allowed in this demo</div>
+            <section className="am-sect">
+                <div className="container-width">
+                    <div className="am-container">
+                        <img alt="" onMouseDown={()=>{ return false; }} src="./img/phone-app.png" className="img-phone" />
+                        <div className="am-content">
+                            <div className="am-pre">ASSET MANAGER</div>
+                            <div className="am-title">Manage your images with Asset Manager</div>
+                            <div className="am-desc">You can create image blocks with the command from the left panel and edit them with double click</div>
+                            <div className="am-post">Image uploading is not allowed in this demo</div>
+                        </div>
                     </div>
                 </div>
+            </section>
+
+            <section className="blk-sect">
+                <Features />
+            </section>
+
+            <section className="bdg-sect">
+                <BoardMembers />
+            </section>
+
+            <Footer /> 
             </div>
-        </section>
-
-        <section className="blk-sect">
-           <Features />
-        </section>
-
-        <section className="bdg-sect">
-            <BoardMembers />
-        </section>
-        
-        <Footer /> 
-    </div>
-  );
+        );
+    }
 }
 
 export default App;
